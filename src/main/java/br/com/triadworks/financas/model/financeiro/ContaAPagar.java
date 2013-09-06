24dfdab8a513923ae8520940efab4e924ba72ac9
Rafael Ponte <rponte@gmail.com>
2013-09-06 18:14:25 -0300

    Mapeia movimentacoes na ContaAPagar;

--- src/main/java/br/com/triadworks/financas/model/financeiro/ContaAPagar.java
+++ src/main/java/br/com/triadworks/financas/model/financeiro/ContaAPagar.java
@@ -1,7 +1,9 @@
 package br.com.triadworks.financas.model.financeiro;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,7 +12,9 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -36,5 +40,9 @@ public class ContaAPagar {
 	
 	@Temporal(TemporalType.DATE)
 	private Date emitidoEm;
+	
+	@OneToMany
+	@JoinColumn(name="conta_id")
+	private List<Movimentacao> movimentacoes = new ArrayList<Movimentacao>();
 	
 }
